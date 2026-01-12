87661f871e6977edb3984cfbf571caf0800d5256
shaziya <shaziyas638@gmail.com>
2026-01-12 11:10:03 +0530

    add two numbers

--- welcome.py
+++ welcome.py
@@ -1,1 +1,8 @@
 print("welcome shaziya")
+
+nums = [2,7,11,15]
+target = 9
+for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    print([i,j])
